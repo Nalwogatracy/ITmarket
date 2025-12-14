6bc8ddde738d895759937e7f4ee40604bd88fb83
nalwoga <nalwoga@nalwogatracy>
2025-12-14 22:12:30 +0300

    First version of shop&sell

--- src/main/java/com/IT_market/ItMarketApplication.java
+++ src/main/java/com/IT_market/ItMarketApplication.java
@@ -3,8 +3,8 @@ package com.IT_market;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.mindrot.jbcrypt.BCrypt;
+
 
 @SpringBootApplication
 public class ItMarketApplication {
@@ -12,9 +12,17 @@ public class ItMarketApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ItMarketApplication.class, args);
 	}
-        @Bean
+        
         public PasswordEncoder passwordEncoder() {
-            return new BCryptPasswordEncoder();
-        }
+            return new PasswordEncoder() {
+                public String encode(CharSequence rawPassword) {
+                    return BCrypt.hashpw(rawPassword.toString(), BCrypt.gensalt());
+                }
 
+                public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                    return BCrypt.checkpw(rawPassword.toString(), encodedPassword);
+                }
+            };
+
+    } 
 }
