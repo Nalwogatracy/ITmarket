cd476f57f5712b8f32d4d9d199a6a80ac4c16459
nalwoga <nalwoga@nalwogatracy>
2025-12-16 19:56:21 +0300

    First version of shop&sell

---
+++ src/main/java/com/IT_market/controller/HomeController.java
@@ -0,0 +1,15 @@
+
+package com.IT_market.controller;
+
+// In your controller package
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "index"; // This looks for src/main/resources/templates/index.html
+    }
+}
