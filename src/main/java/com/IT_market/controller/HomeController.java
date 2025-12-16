2fef843b32d0f7873c2a183731bb266d597b625a
nalwoga <nalwoga@nalwogatracy>
2025-12-16 20:48:41 +0300

    First version of shop&sell

--- src/main/java/com/IT_market/controller/HomeController.java
+++ src/main/java/com/IT_market/controller/HomeController.java
@@ -4,10 +4,17 @@ package com.IT_market.controller;
 // In your controller package
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     
+    @GetMapping("/health")
+    @ResponseBody
+    public String health() {
+        return "OK";
+    }
+      
     @GetMapping("/")
     public String home() {
         return "index"; // This looks for src/main/resources/templates/index.html
